cb688ad3ced38ec1a4b12b6f80a92579a873bd68
Giarlan Santana <giarlansilva@yahoo.com>
2023-04-21 21:18:15 -0300

    IMPLEMENT NEW FEATURES
       *Build - It has been created the 'questions.js' file and the questions code
       *Build - The 'question.js' file has been linked in the HTML

---
+++ assets/js/questions.js
@@ -0,0 +1,104 @@
+let questions = [
+    {
+        question: 'What does CSS stand for?',
+        options: [
+            'Creative Style Sheets',
+            'Cascading Style Sheets',
+            'Colorful Style Sheets',
+            'Computer Style Sheets'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Where is the right place to put the javascript?',
+        options: [
+            'In the body tag',
+            'In the head tag',
+            'Both the body and head tags are correct'
+        ],
+        answer: 2
+    },
+    {
+        question: 'What does HTML stand for?',
+        options: [
+            'Hyper Text Markup Language',
+            'Home Tool Markup Language',
+            'Hyperlinks And Text Markup Language'
+        ],
+        answer: 0
+    },
+    {
+        question: 'An external javascript file needs to have the script tag',
+        options: [
+            'True',
+            'False'
+        ],
+        answer: 1
+    },
+    {
+        question: "How's the correct way to write \"Hello, world\" into an alert box?",
+        options: [
+            'alertBox("Hello World")',
+            'msgBox("Hello World")',
+            'msg("Hello World")',
+            'alert("Hello World")'
+        ],
+        answer: 3
+    },
+    {
+        question: 'How to create a function in Javascript?',
+        options: [
+            'function:myFunction()',
+            'function = myFunction()',
+            'function myFunction()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'How to call a function named "myFunction"?',
+        options: [
+            'call myFunction',
+            'call function myFunction',
+            'myFunction()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'How to write an IF conditional in JavaScript?',
+        options: [
+            'if i = 5',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i = 5 then'
+        ],
+        answer: 2
+    },
+    {
+        question: 'What is Git?',
+        options: [
+            'A programing language',
+            'A version control system',
+            'A nickname for GitHub',
+            'A remote repository platform'
+        ],
+        answer: 1
+    },
+    {
+        question: 'How does the while loop start?',
+        options: [
+            'while (i <= 10)',
+            'while (i <= 10; i++)',
+            'while i = 1 to 10'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Did you like the project?',
+        options: [
+            'Yes!',
+            'No!',
+            'Maybe'
+        ],
+        answer: 0
+    },
+];
